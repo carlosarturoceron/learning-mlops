d2bf488a07ea066832b348c2c2530c9859e0a5a7
Carlos Ceron <97488732+carlosarturoceron@users.noreply.github.com>
2024-05-28 05:02:32 +0000

    enable autolog

--- 02-exp-tracking/02-hw/homework/train.py
+++ 02-exp-tracking/02-hw/homework/train.py
@@ -5,6 +5,8 @@ import click
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+import mlflow
+
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -19,14 +21,18 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run():
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        mlflow.sklearn.autolog()
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
 
 if __name__ == '__main__':
