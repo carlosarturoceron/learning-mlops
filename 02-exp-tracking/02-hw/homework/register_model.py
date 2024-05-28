fe1022ce48b3005f41502f305106a8c950759fa1
Carlos Ceron <97488732+carlosarturoceron@users.noreply.github.com>
2024-05-28 05:51:20 +0000

    finished HW2

--- 02-exp-tracking/02-hw/homework/register_model.py
+++ 02-exp-tracking/02-hw/homework/register_model.py
@@ -9,7 +9,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
 HPO_EXPERIMENT_NAME = "random-forest-hyperopt"
-EXPERIMENT_NAME = "random-forest-best-models"
+EXPERIMENT_NAME = "random-forest-best-modelsss"
 RF_PARAMS = ['max_depth', 'n_estimators', 'min_samples_split', 'min_samples_leaf', 'random_state']
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
@@ -71,10 +71,16 @@ def run_register_model(data_path: str, top_n: int):
 
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=top_n,
+        order_by=["metrics.rmse ASC"]
+    )[0].info.run_id
 
     # Register the best model
-    # mlflow.register_model( ... )
+    model_uri = f'runs:/{best_run}/model'
+    mlflow.register_model(model_uri=model_uri, name='nyc-taxi-regressor-cli')
 
 
 if __name__ == '__main__':
